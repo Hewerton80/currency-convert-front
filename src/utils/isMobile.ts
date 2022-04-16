f44b37fe6bc30538d8f50714e28b26885b787dac
hewerton <hewerton80@gmail.com>
2022-04-16 14:56:00 -0300

    remove no-used files

--- src/utils/isMobile.ts
+++
@@ -1,8 +0,0 @@
-import { isUndefined } from './isType'
-import assets from '../../assets.json'
-export const isMobile = () => {
-  if (!isUndefined(window?.innerWidth)) {
-    return window.innerWidth < assets.breakpoints.md
-  }
-  return false
-}
--- src/utils/postStatusOptions.ts
+++
@@ -1,6 +0,0 @@
-import { PostStatusEnum, PostStatusPtBrEnum } from '../types/Post'
-
-export const postStatusOptions = Object.values(PostStatusEnum).map((postStatus) => ({
-  value: postStatus,
-  text: PostStatusPtBrEnum[postStatus],
-}))
